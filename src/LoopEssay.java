7b0100d35b5125afa1e16754c813db678802fc62
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-06-27 22:12:24 -0700

    added essay

--- src/LoopEssay.java
+++ src/LoopEssay.java
@@ -2,7 +2,35 @@
 public class LoopEssay {
 
 	public static void main(String[] args) {
-		
+		/*
+		 * LOOPS
+		 * 
+		 * While Loop
+		 * The while loop runs while a condition is true. The condition is evaluated and the loop will
+		 * will continue to run until the condition is no longer true. If the condition is not true or 
+		 * is never true, the while loop will not run. This type of loop can be used when you want a loop
+		 * to run until a certain condition is true.
+		 * 
+		 * 
+		 * For Loop
+		 * The for loop is used to loop through an object or array and execute instructions written inside
+		 *  the scope of the loop. The for loop has 3 statements that set up and oversee the function of 
+		 *  the loop. The first statement in the loop sets the iterating variable to the 
+		 * starting number. This variable is often called i and set to a value of 0. The second statement 
+		 * in the for loop is the exit statement or the information that tells the loop how many times to 
+		 * iterate through the object. The third statement is the iterator itself. Once the first loop
+		 * is executed, the iterator will increase the variable by the number assigned in the iterator 
+		 * statement. This is usually written as i++ which increases i by one each time a loop is completed.
+		 * 
+		 * Do While Loop
+		 * The do while loop differs from the while loop in one major way. The while loop will only run if 
+		 * its condition is true and stops when another condition is met. The do while loop runs once before
+		 * any conditions are evaluated. This is not a common loop and is only used when you need an action
+		 * performed at least once before any conditions are met or evaluated. Once the first iteration of 
+		 * loop is performed, the condition is evaluated and , like the while loop, it either runs while its
+		 * condition is true or doesn't run at all.
+		 * 
+		 */
 
 	}
 
